3f7c883055654625a6cbe3796bc3cd6a418a23fd
Abdymambetov <meder0555@icloud.com>
2023-03-06 19:45:57 +0600

    add: validation

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import JustHeader from './justHeader/JustHeader'
+import SignUpHeader from './signUpHeader/SignUpHeader'
 
 function Header() {
     return (
         <>
-            {/* <SignUpHeader/>    */}
-            <JustHeader />
+            <SignUpHeader/>   
+            {/* <JustHeader /> */}
         </>
     )
 }
