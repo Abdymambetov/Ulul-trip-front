100d3344cc278719fce65892d12243814d45e936
Abdymambetov <meder0555@icloud.com>
2023-03-11 16:50:52 +0600

    add: tourSlice

--- src/links/TourLinks.js
+++ src/links/TourLinks.js
@@ -1,7 +1,8 @@
 export const tourLinks ={
-    category: 'http://164.92.190.147:8880/tour/src/v1/categories/',
-    guides: 'http://164.92.190.147:8880/tour/src/v1/guides/',
-    regions: 'http://164.92.190.147:8880/tour/src/v1/regions/',
-    review: 'http://164.92.190.147:8880/tour/src/v1/review/',
-    tour: 'http://164.92.190.147:8880/tour/src/v1/tours/',
+    category: 'http://164.92.190.147:8880/home/categories/',
+    guides: 'http://164.92.190.147:8880/home/guides/',
+    regions: 'http://164.92.190.147:8880/home/regions/',
+    review: 'http://164.92.190.147:8880/home/review/',
+    tour: 'http://164.92.190.147:8880/home/tours/',
+    slugs: 'http://164.92.190.147:8880/home/slugs/',
 }
