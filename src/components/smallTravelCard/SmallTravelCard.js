d141bddb608080628760bc7e70bd4dfd395da9cf
Abdymambetov <meder0555@icloud.com>
2023-03-12 14:29:11 +0600

    register

--- src/components/smallTravelCard/SmallTravelCard.js
+++ src/components/smallTravelCard/SmallTravelCard.js
@@ -7,15 +7,9 @@ import star from '../../images/smallCardImg/star.svg'
 import { useDispatch } from 'react-redux'
 import { openCardModal } from '../../store/slices/authSlice'
 
-<<<<<<< HEAD
-function SmallTravelCard({item}) {
-  const [active ,setActive] = useState(false);
-  const [notActive , setNotActive] =useState('heard');
-=======
 function SmallTravelCard() {
     const [active, setActive] = useState(false)
     const [notActive, setNotActive] = useState('heard')
->>>>>>> 830d5119bf0914e0d6a36321f09c35a4c14b2b27
 
     const heandleClick = e => {
         if (active === false) {
@@ -40,31 +34,6 @@ function SmallTravelCard() {
             dispatch(openCardModal())
         }
     }
-<<<<<<< HEAD
-  }
-  const dispatch = useDispatch()
-    const openModal = () => {
-        dispatch(openCardModal())
-    }
-     
-  return (
-    <div className={classes.small_card} onClick={openModal}>
-       <div className={classes.parent_card}>
-        <div className={classes.photo_block}>
-          <div className='photo'>
-            <img src={item?.tour_images[0]?.images.replace(
-                                    /(\d{1,3}\.\d{1,3}\.\d{1,3}\.)\d{1,3}/,
-                                    '$1' + '147:8880'
-                                ) } alt="car" className={classes.card_img} />
-            <div className={notActive} style={{width: "32px", height: "32px", }}>
-                <Heart  className='heard-back' 
-                  isActive={active} 
-                  onClick={heandleClick}
-                  animationTrigger = "both" 
-                  inactiveColor='#FF6F32'
-                  activeColor = "#FF6F32"
-                  animationDuration={0.1} style={{width: '20px', height: '18px'}}/>
-=======
     return (
         <div
             className={classes.small_card}
@@ -131,40 +100,9 @@ function SmallTravelCard() {
                         </div>
                     </div>
                 </div>
->>>>>>> 830d5119bf0914e0d6a36321f09c35a4c14b2b27
             </div>
         </div>
-<<<<<<< HEAD
-        <div className={classes.locatiion_tour}>
-          <p className={classes.location_tour_text}>{item?.title}</p>
-        </div>
-        <div className={classes.location_info}>
-          <div className={classes.location_region}>
-            <div className={classes.region_tour}>
-              <p className={classes.region_text}>{item?.region[0]?.name}</p>
-              <img src={point} alt="point" className={classes.region_img}/>
-            </div>
-            <div className={classes.tour_price}>
-                <p className={classes.price_number}>{item?.price} <span className={classes.price_text}>сом</span></p>
-                
-            </div>
-            <div className={classes.tour_day}>
-                <p className={classes.day_number}>{item?.duration}  <span className={classes.day_text}>день</span></p>
-            </div>
-          </div>
-          <div className={classes.star_parent}>
-            <div className={classes.card_star}>
-              <img src={star} alt="star" />
-              <p className={classes.rating_text}>{item?.average_rating}</p>
-            </div>
-          </div>
-        </div>
-       </div>
-    </div>
-  )
-=======
     )
->>>>>>> 830d5119bf0914e0d6a36321f09c35a4c14b2b27
 }
 
 export default SmallTravelCard
